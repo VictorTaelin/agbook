52bfad6974fb276e6ee27e69c0d5629016dfc044
Nicolas Abril <nicolas.abril@protonmail.ch>
2024-09-19 17:01:05 +0200

    Update check_all.sh to work on (hopefully) all shells with input path

--- check_all.sh
+++ check_all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-find $1 -name "*.agda" -not -path "*.tmp*" | while read -r file; do
+# First argument or '.' if nothing is passed
+search_path=${1:-.}
+
+find "$search_path" -name "*.agda" -not -path "*.tmp*" | while read -r file; do
     echo "Checking: $file"
     agda-cli check "$file"
     echo ""
