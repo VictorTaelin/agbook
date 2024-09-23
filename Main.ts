835211318c5c9c691082d44f6c15389529d1768b
Victor Taelin <victor.taelin@gmail.com>
2024-09-23 18:57:07 -0300

    Main.ts imports All.ts

--- Main.ts
+++ Main.ts
@@ -1,23 +1,14 @@
-import { IO } from './Base/IO/Type';
-import { Unit, $unit } from './Base/Unit/Type';
-import { $print } from './Base/IO/print';
-import { $bind } from './Base/IO/Monad/bind';
-import { Nat, $Zero } from './Base/Nat/Type';
-import { $add } from './Base/Nat/add';
-import { $show } from './Base/Nat/show';
-import { $append } from './Base/String/append';
+import { Nat, IO, Unit, String } from './Base/ALL';
 
-const $loop = (i: Nat): IO<Unit> => {
-  return $bind(
-    $print($append("Hello ", $show(i))),
-    () => $loop($add(i, 1n))
-  );
+const $loop = (i: Nat): IO<Unit> => async () => {
+  await IO.$print(String.$append("Hello ", Nat.$show(i)))();
+  return $loop(Nat.$add(i, 1n))();
 };
 
-export const loop = (i: Nat) => $loop(i);
+export const $main: IO<Unit> = $loop(0n);
+export const  main: IO<Unit> = $main;
 
-export const $main: IO<Unit> = $loop($Zero);
-export const  main = () => $main;
-
-// To run the main function:
-$main().catch(error => console.error("An error occurred:", error));
+// Run the main function if this is the entry point
+if (require.main === module) {
+  main().catch(console.error);
+}
