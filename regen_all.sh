bf85438bc85f13e89a6c1f872dd69116dfb71ddd
Miëtek Bak <mietek@bak.io>
2024-09-27 09:32:01 +0200

    Add regen_all.sh script to regenerate ALL.agda files

---
+++ regen_all.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# This script regenerates "ALL.agda" files.
+
+# Ensure script exits if any command fails.
+set -e
+
+SEARCH_PATH="${1:-Base}"
+
+# For each first-level directory:
+find -s "${SEARCH_PATH}" -depth 1 -type d |
+  while read -r dir
+do
+  # Replace slashes with periods in directory path to obtain directory's fully-qualified module name.
+  dir_mod="${dir////.}"
+
+  # Start regenerating directory's "ALL.agda".
+  echo -e "module ${dir_mod}.ALL where\n" > "${dir}/ALL.agda"
+
+  # For each "*.agda" file that isn't "ALL.agda" or temporary, and isn't located in "Examples" or "Test":
+  # FIXME: Temporarily skipping "Omega" as well.
+  find -s "${dir}" -type f \
+      -name "*.agda" \
+      -not -name "ALL.agda" \
+      -not -path "*.tmp*" \
+      -not -path "*/Examples/*" \
+      -not -path "*/Omega/*" \
+      -not -path "*/Test/*" |
+    while read -r file
+  do
+    # Remove extension from file path.
+    file_base="${file%%.agda}"
+
+    # Replace slashes with periods in file path to obtain file's fully-qualified module name.
+    file_mod="${file_base////.}"
+
+    # Add file to directory's "ALL.agda".
+    echo "open import ${file_mod} public" >> "${dir}/ALL.agda"
+  done
+done
