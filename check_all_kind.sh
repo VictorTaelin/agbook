965787dcfdb18453285edd5a0efcb123bc63a953
Victor Taelin <victor.taelin@gmail.com>
2024-09-29 20:19:38 -0300

    update the kind all checker

--- check_all_kind.sh
+++ check_all_kind.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-
-for file in $(find . -name "*.kind" -not -path "./node_modules/*" -not -path "./Test/*"); do
-  echo "Checking $file..."
-  if ! kind check "$file"; then
-    echo "Removing $file"
-    rm "$file"
-  fi
-done
-
+kind check
