7eb8a5a1fe2883073b38d8c12b366e02b0efaf87
kings177 <yanmendesp@gmail.com>
2024-10-01 21:31:13 -0300

    fix locale env and add check all kind script

--- check_all_kind.sh
+++ check_all_kind.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
-kind check
+
+# Checks all Kind files and outputs checks normally.
+SEARCH_PATH="${1:-./Base}"
+
+check_file() {
+    file="$1"
+    kind check "${file}"
+}
+
+main() {
+    while IFS= read -r -d '' file; do
+        check_file "${file}"
+    done < <(find "${SEARCH_PATH}" -name "*.kind" -not -path "*/Test/*" -print0)
+    echo "All Kind checks completed."
+}
+
+main "$@"
